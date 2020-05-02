c0e5192813970c553d69123d10fef9b3648364d0
Sospeter Kungu <sospeter2030@gmail.com>
2020-05-02 10:47:46 +0300

    initial commit. added the scripts.js and the functions to convert celsius to degrees

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var add = function(number1, number2){
+  return number1 + number2;
+}
+var number3 = parseFloat(prompt("Enter Temperature in degrees celcius;"));
+var number4 = 1.8;
+var number2 = 32;
+var number1 = parseFloat(number3 * number4);
+var number5 = parseFloat(number1 + number2);
+alert("Your conversion of " + number3 + " degrees Celcius to degrees Fahrenheit is " + number5 + " degrees Fahrenheit");
